4d3d1c950447640ac4c0de17101a05714d533f08
Abhishek17abk <64626877+Abhishek17abk@users.noreply.github.com>
2024-09-14 00:55:46 -0400

    Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ function Home() {
       title: "Football's popularity, cardiac safety awareness opportunity",
       excerpt: "Medics see football's heart problem as chance to..",
       imageUrl: '../images/Gemini_Generated_Image_yezhluyezhluyezh.jpeg', 
-      pdfUrl: `${process.env.PUBLIC_URL}../pdfs/Features/Football's popularity, cardiac safety awareness opportunity.pdf`
+      link: `../pdfs/Features/Football's popularity, cardiac safety awareness opportunity.pdf`
     },
     {
       id: 3,
